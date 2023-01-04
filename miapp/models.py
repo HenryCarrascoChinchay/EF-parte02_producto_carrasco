267cf8df16ccf201df6f6fc681610cf84ddbdf40
Henry Carrasco <1913010968@untels.edu.pe>
2023-01-04 11:11:43 -0500

    EF Parte02 - Producto - Carrasco

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Docente(models.Model):
+    codigo = models.CharField(max_length=150)
+    nombre = models.TextField()
+    apellido_paterno = models.TextField()
+    apellido_materno = models.TextField()
+    dni = models.CharField(max_length=8)
+    publicado = models.BooleanField()
+    # auto_now_add me permitirá registrar
+    # la fecha cuando cree el registro
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    fecha_nacimiento = models.DateTimeField(auto_now=True)
+    estado = models.TextField()
+    
